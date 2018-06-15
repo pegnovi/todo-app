620a21b0db572da10afd8d11bdd001c04ec25e11
Patrick Ocampo <pegnovi@yahoo.com>
2018-06-15 15:04:45 -0700

    Added server-side express routes for todo CRUD ops.

--- server/routes.js
+++ server/routes.js
@@ -1,51 +1,90 @@
-// const pg = require('pg');
-
-// console.log(process.env);
-
-// const pool = new pg.Pool({
-// 	user: process.env.PG_USER,
-// 	password: process.env.PG_PASSWORD,
-// 	database: process.env.PG_DATABASE,
-// 	host: process.env.PG_HOST,
-// 	max: 10, // max number of clients in pool
-// 	idleTimeoutMillis: 1000,
-// 	port: process.env.PG_PORT
-// });
+const pgp = require('pg-promise')({});
+const pgpConfig = {
+	user: process.env.PG_USER,
+	password: process.env.PG_PASSWORD,
+	database: process.env.PG_DATABASE,
+	host: process.env.PG_HOST,
+	poolSize: 10, // max number of clients in pool
+	idleTimeoutMillis: 1000,
+	port: process.env.PG_PORT
+};
+const db = pgp(pgpConfig);
 
 module.exports = function(app) {
 
 	app.post('/api/todo', (req, res) => {
+		db.one('INSERT INTO todos (task) VALUES (${task}) RETURNING id',
+			{
+				task: req.body.task
+			}
+		)
+		.then(todoId => {
+			console.log(todoId);
+			res.status(201).json(todoId);
+		})
+		.catch(error => {
+			console.log(error);
+			res.status(400).send(error);
+		});
+	});
 
-		res.send('created todo');
+	//https://stackoverflow.com/questions/43208713/express-get-route-will-not-work-with-parameters
+	//https://stackoverflow.com/questions/30967822/when-do-i-use-path-params-vs-query-params-in-a-restful-api
+	app.get('/api/todo/:id', (req, res) => {
+		db.one('SELECT id, task FROM todos WHERE id=${id}',
+			{
+				id: req.params.id
+			}
+		)
+		.then(todo => {
+			res.status(200).json(todo);
+		})
+		.catch(error => {
+			console.log(error);
+			res.status(400).send(error);
+		});
+	});
 
-		// pool.connect((err, client, release) => {
-		// 	if(err) {
-		// 		throw err;
-		// 	}
+	app.get('/api/todos', (req, res) => {
+		db.any('SELECT id, task FROM todos')
+		.then(todos => {
+			res.status(200).json(todos);
+		})
+		.catch(error => {
+			console.log(error);
+			res.status(400).send(error);
+		});
+	});
 
-		// 	console.log(req.query);
+	app.delete('/api/todo', (req, res) => {
+		db.none('DELETE FROM todos WHERE id=${id}',
+			{
+				id: req.body.id
+			}
+		)
+		.then(() => {
+			res.status(200).send('OK');
+		})
+		.catch(error => {
+			console.log(error);
+			res.status(400).send(error);
+		});
+	});
 
-		// 	querystring = `INSERT INTO services (id, name, description, price)
-		// 		VALUES (
-		// 			'${req.query.id}',
-		// 			'${req.query.name}',
-		// 			'${req.query.description}',
-		// 			'${parseFloat(req.query.price)}'
-		// 		)
-		// 	`;
-
-		// 	client.query(querystring, (err, result) => {
-		// 		if(err) {
-		// 			throw err;
-		// 		}
-
-		// 		// was successful
-		// 		release();
-		// 		if(result && result.rows) {
-		// 			res.send('created todo');
-		// 		}
-		// 	});
-		// });
+	app.put('/api/todo', (req, res) => {
+		db.none('UPDATE todos SET task=${task} WHERE id=${id}',
+			{
+				task: req.body.task,
+				id: req.body.id
+			}
+		)
+		.then(() => {
+			res.status(200).send('OK');
+		})
+		.catch(error => {
+			console.log(error);
+			res.status(400).send(error);
+		});
 	});
 
 };
--- server/scripts/recreate-db.sql
+++ server/scripts/recreate-db.sql
@@ -5,6 +5,5 @@ DROP TABLE IF EXISTS todos;
 CREATE TABLE todos(
 	id uuid DEFAULT uuid_generate_v4 (),
 	task text,
-	sub_tasks text[],
 	PRIMARY KEY (id)
 );
